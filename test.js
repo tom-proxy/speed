d98d88174a6bb7d10c99c72ff78722e0babe0dc8
湯姆 <89062432+tom-proxy@users.noreply.github.com>
2023-07-29 22:42:49 +0800

    Update test.js

--- test.js
+++ test.js
@@ -30,7 +30,7 @@ let content = ''
   const bytes = mb * 1024 * 1024
   let start = Date.now()
   const res = await $.http.get({
-    url: `http://captive.apple.com/__down?bytes=${bytes}`
+    url: `http://www.apple.com/__down?bytes=${bytes}`
   })
   const end = Date.now()
   const duration = (end - start) / 1000
